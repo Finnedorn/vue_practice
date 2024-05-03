0d068eb15467f025a2ce7040ac5d02e5d9f98e4c
Finnedorn <daniele.giuntoli@outlook.com>
2024-05-03 17:00:02 +0200

    Basic Scaffholding

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <HeaderComponent/>
+  <router-view :key="$route.path"></router-view>
+</template>
+
+<script>
+import axios from "axios";
+import { store } from "./assets/data/store";
+import HeaderComponent from "./components/HeaderComponent.vue";
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+// @use "./assets/style/partials/variables" as *;
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item" v-for="item in menu" :key="item.index">
+                            <router-link class="nav-link" active-class="active" :to="{name: item.name}">{{item.label}}</router-link>
+                        </li>
+                    </ul> -->
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+
+export default {
+    name: "HeaderComponent",
+    components: {
+    },
+    data() {
+        return {
+            menu: [
+                {
+                    label: "Home",
+                    name: "home",
+                },
+                {
+                    label: "Projects",
+                    name: "projects",
+                },
+                {
+                    label: "Contacts",
+                    name: "contacts",
+                }
+            ]
+        }
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
